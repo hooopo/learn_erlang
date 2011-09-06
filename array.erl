f1233f465a8d4bd878337d8768cb68d168dd7b02
Hooopo <hoooopo@gmail.com>
2011-09-07 05:14:05 +0800

    rm old file

--- array.erl
+++
@@ -1,5 +0,0 @@
--module(array).
--export([my_length/1]).
-
-my_length([]) -> 0;
-my_length([First|Rest]) -> 1 + my_length(Rest).
--- my_io.erl
+++
@@ -1,8 +0,0 @@
--module(my_io).
--export([print_list/1]).
-
-%%TODO print list which's item is num.
-print_list([]) -> ok;
-print_list([Item|Rest]) ->
-  io:format(Item, []),
-  print_list(Rest).
