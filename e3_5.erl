9c7c7ecf6a03d3d54b5330f396e199ad6e630877
Hooopo <hoooopo@gmail.com>
2011-09-10 02:00:11 +0800

    e3_5 reverse

--- e3_5.erl
+++ e3_5.erl
@@ -1,6 +1,9 @@
 -module(e3_5).
--export([filter/2]).
+-export([filter/2, reverse/1]).
 
 filter([], _Item) -> [];
 filter([Head|Tail], Item) when Item >= Head -> [Head|filter(Tail, Item)];
 filter([Head|Tail], Item) when Item < Head -> filter(Tail, Item).
+
+reverse([]) -> [];
+reverse([Head|Tail]) -> reverse(Tail) ++ [Head].
