6cd9b5fc41f2ae173f819fdd13e6770bc6756233
Hooopo <hoooopo@gmail.com>
2011-09-12 08:52:00 +0800

    merge sort

--- e3_6.erl
+++ e3_6.erl
@@ -1,6 +1,22 @@
 -module(e3_6).
--export([quicksort/1]).
+-export([quick_sort/1, merge_sort/1, merge/3]).
 
-quicksort([]) -> [];
-quicksort([H|T]) -> {Left, Right} = lists:partition(fun(X) -> X =< H end, T),
-  quicksort(Left) ++ [H] ++ quicksort(Right).
+quick_sort([]) -> [];
+quick_sort([H|T]) -> 
+  {Left, Right} = lists:partition(fun(X) -> X =< H end, T),
+  quick_sort(Left) ++ [H] ++ quick_sort(Right).
+
+merge_sort([]) -> [];
+merge_sort([Single]) -> [Single];
+merge_sort(List) -> 
+  {Left, Right} = lists:split(length(List) div 2, List),
+  merge(merge_sort(Left), merge_sort(Right), []).
+
+merge([], [], Result) -> Result;
+merge([], Right, Result) -> merge([], [], Result ++ Right);
+merge(Left, [], Result) -> merge([], [], Result ++ Left);
+merge([HL|TL], [HR|TR], Result) -> 
+  if 
+    HL =< HR -> merge(TL, [HR|TR], Result ++ [HL]);
+    HL > HR  -> merge([HL|TL], TR, Result ++ [HR])
+  end.
